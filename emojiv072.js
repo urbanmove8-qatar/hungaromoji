791a235b381b4a0574246fc14615dc0b7175eb24
Urbanmove 8 Team <robloxurbanmove8@gmail.com>
2025-09-08 09:44:12 +0300

    Create emojiv072.js

---
+++ emojiv072.js
@@ -0,0 +1,183 @@
+(function() {
+    // Hungaromoji v0.7.2
+    // Copyright © 2023–2025 Urbanmove 8 Qatar Sole Prop.
+    // Emoji assets © Penguinmod.com. All rights reserved.
+    // Some book excerpts © Móra Könyvkiadó and feketeistvan.hu. Minden jog fenntartva.
+    // OMSZ logo © Wikimedia.org. All rights reserved.
+    // Budapest Metro logo assets © Budapesti Közlekedési Központ Zrt. Minden jog fenntartva.
+    // 100 Hungarian Forints image © Magyar Nemzeti Bank. Minden jog fenntartva.
+    
+    //Copyright 2025 Urbanmove 8
+
+    //Licensed under the Apache License, Version 2.0 (the "License");z
+  
+    const emojiMap = {
+        "👩": "https://library.penguinmod.com/files/emojis/woman.png",
+        "👨": "https://library.penguinmod.com/files/emojis/man.png",
+        "👧": "https://library.penguinmod.com/files/emojis/girl.png",
+        "👦": "https://library.penguinmod.com/files/emojis/boy.png",
+        "💀": "https://library.penguinmod.com/files/emojis/skull.png",
+        "😀": "https://library.penguinmod.com/files/emojis/animatedsmile.png",
+        "😭": "https://library.penguinmod.com/files/emojis/sobbing.png",
+        "💻": "https://library.penguinmod.com/files/emojis/laptop.png",
+        "🎉": "https://library.penguinmod.com/files/emojis/tada.png",
+        "🥳": "https://library.penguinmod.com/files/emojis/party.png",
+        "🚗": "https://library.penguinmod.com/files/emojis/car.png",
+        "🤓": "https://library.penguinmod.com/files/emojis/nerd.png",
+        "👉": "https://library.penguinmod.com/files/emojis/pointright.png",
+        "👈": "https://library.penguinmod.com/files/emojis/pointleft.png",
+        "👇": "https://library.penguinmod.com/files/emojis/pointdown.png",
+        "🫵": "https://library.penguinmod.com/files/emojis/pointatyou.png",
+        "☝️": "https://library.penguinmod.com/files/emojis/pointup.png",
+        "🗿": "https://library.penguinmod.com/files/emojis/moai.png",
+        "📱": "https://library.penguinmod.com/files/emojis/phone.png",
+        "🪙": "https://urbanmove8.neocities.org/kekhegy/kepek/100-forintos.png",
+        "🌉": "https://urbanmove8.neocities.org/kekhegy/kepek/lanchid.png",
+        "📗": "https://www.feketeistvan.hu/wp-content/uploads/2024/07/konyborito_mora_tuskevar.png",
+        "📙": "https://mora.hu/content/2021/3/Product/fekete_istvan_vuk.png",
+        "📕": "https://upload.wikimedia.org/wikipedia/commons/d/dd/Egricsillagok.jpg",
+        "🙂": "https://library.penguinmod.com/files/emojis/smile.png",
+        "🤡": "https://library.penguinmod.com/files/emojis/clown.png",
+        "🤑": "https://library.penguinmod.com/files/emojis/eyesofmoney.png",
+        "🙄": "https://library.penguinmod.com/files/emojis/rollingeyes.png",
+        "😍": "https://library.penguinmod.com/files/emojis/hearteyes.png",
+        "😞": "https://library.penguinmod.com/files/emojis/animatedfrown.png",
+        "😫": "https://library.penguinmod.com/files/emojis/annoyed.png",
+        "🇱🇺": "https://urbanmove8.neocities.org/images/flags/luxemburg.png",
+        "🇭🇺": "https://urbanmove8.neocities.org/images/flags/hungary.png",
+        "🇵🇱": "https://urbanmove8.neocities.org/images/flags/poland.png",
+        "🇨🇿": "https://urbanmove8.neocities.org/images/flags/czech.png",
+        "🇦🇹": "https://urbanmove8.neocities.org/images/flags/austria.png",
+        "🇪🇺": "https://urbanmove8.neocities.org/images/flags/eu.png",
+        "🌃": "https://urbanmove8.neocities.org/kekhegy/kepek/orszaghaz.png",
+        "‍⚕️": "https://upload.wikimedia.org/wikipedia/commons/3/34/Insignia_Hungary_OMSZ_v1.svg",
+        "🤷": "https://library.penguinmod.com/files/emojis/idk.png",
+        "🍫": "https://urbanmove8.neocities.org/kekhegy/kepek/sportcsoki.png",
+        "🔞": "https://urbanmove8.neocities.org/kekhegy/kepek/18plusz.png",
+        "🚌": "https://urbanmove8.neocities.org/kekhegy/kepek/ikarus260.png",
+        "🚂": "https://urbanmove8.neocities.org/kekhegy/kepek/mav43.png",
+        "Ⓜ️": "https://upload.wikimedia.org/wikipedia/commons/f/fb/BKV_metro.svg",
+        "M1": "https://urbanmove8.neocities.org/kekhegy/kepek/metro1.png",
+        "M2": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/BKV_m_2_jms.svg/800px-BKV_m_2_jms.svg.png",
+        "M3": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/BKV_m_3_jms.svg/800px-BKV_m_3_jms.svg.png",
+        "M4": "https://urbanmove8.neocities.org/kekhegy/kepek/metro4.png",
+        "🚇": "https://urbanmove8.neocities.org/kekhegy/kepek/budapestim3asmetroszerelveny.png",
+        "💶": "https://urbanmove8.neocities.org/emojis/stackedeuros.png",
+        "💵": "https://urbanmove8.neocities.org/emojis/stackeddollars.png",
+        "💴": "https://urbanmove8.neocities.org/emojis/stackedyens.png",
+        "☹️": "https://library.penguinmod.com/files/emojis/bigfrown.png",
+        "😧": "https://library.penguinmod.com/files/emojis/scared.png",
+        "🧐": "https://library.penguinmod.com/files/emojis/investigate.png",
+        "😠": "https://library.penguinmod.com/files/emojis/angry.png",
+        "😲": "https://library.penguinmod.com/files/emojis/astonishment.png",
+        "😨": "https://library.penguinmod.com/files/emojis/shocked.png",
+        "👶": "https://library.penguinmod.com/files/emojis/baby.png",
+        "🏳️": "https://library.penguinmod.com/files/emojis/flagwhite.png",
+        "🚩": "https://library.penguinmod.com/files/emojis/flagred.png",
+        "🏁": "https://library.penguinmod.com/files/emojis/flagcheck.png",
+        "🏴": "https://library.penguinmod.com/files/emojis/flagblack.png",
+        "😎": "https://library.penguinmod.com/files/emojis/cool.png",
+        "🥶": "https://library.penguinmod.com/files/emojis/cold.png",
+        "😈": "https://library.penguinmod.com/files/emojis/devil.png",
+        "😵": "https://library.penguinmod.com/files/emojis/expressivedead.png",
+        "😶‍🌫️": "https://library.penguinmod.com/files/emojis/faceinclouds.png",
+        "💪": "https://library.penguinmod.com/files/emojis/flex.png",
+        "😡": "https://library.penguinmod.com/files/emojis/furious.png",
+        "🎂": "https://library.penguinmod.com/files/emojis/cake.png",
+        "🔔": "https://library.penguinmod.com/files/emojis/bell.png",
+        "🔕": "https://library.penguinmod.com/files/emojis/bellno.png",
+        "🤮": "https://library.penguinmod.com/files/emojis/barf.png",
+        "🤢": "https://library.penguinmod.com/files/emojis/badtaste.png",
+        "👋": "https://library.penguinmod.com/files/emojis/animatedwave.png",
+        "👽": "https://library.penguinmod.com/files/emojis/alien.png",
+        "🎆": "https://urbanmove8.neocities.org/kekhegy/kepek/szntistvannapjatuzijatekok.png",
+        "📰": "https://urbanmove8.neocities.org/kekhegy/kepek/magyarnemzetujsagutolso.png",
+        "😂": "https://library.penguinmod.com/files/emojis/laughing.png",
+        "🚓": "https://urbanmove8.neocities.org/emojis/rendorseg.png",
+        "😊": "https://library.penguinmod.com/files/emojis/blush.png",
+        "🤠": "https://library.penguinmod.com/files/emojis/cowboy.png",
+        "🍔": "https://library.penguinmod.com/files/emojis/burger.png",
+        "🫧": "https://library.penguinmod.com/files/emojis/bubbles.png",
+        "😄": "https://library.penguinmod.com/files/emojis/casualjoy.png",
+        "😝": "https://library.penguinmod.com/files/emojis/bleh.png",
+        "🫡": "https://library.penguinmod.com/files/emojis/salute.png",
+        "😇": "https://library.penguinmod.com/files/emojis/angel.png",
+        "😬": "https://library.penguinmod.com/files/emojis/grimacing.png",
+        "😟": "https://library.penguinmod.com/files/emojis/worried.png",
+        "🫤": "https://library.penguinmod.com/files/emojis/meh.png",
+        "🔊": "https://urbanmove8.neocities.org/emojis/volumeloud.png",
+        "🔇": "https://urbanmove8.neocities.org/emojis/volumeoff.png",
+        "🙍": "https://urbanmove8.neocities.org/emojis/personfrown.png",
+        "🙎": "https://urbanmove8.neocities.org/emojis/personpouting.png",
+        "😾": "https://library.penguinmod.com/files/emojis/catangry.png",
+        "😸": "https://library.penguinmod.com/files/emojis/cathappy.png",
+        "😻": "https://library.penguinmod.com/files/emojis/cathearteyes.png",
+        "🙀": "https://library.penguinmod.com/files/emojis/catscream.png",
+        "🐶": "https://library.penguinmod.com/files/emojis/dog.png",
+        "🚫": "https://library.penguinmod.com/files/emojis/ocross.png",
+        "🪛": "https://library.penguinmod.com/files/emojis/screwdriver.png",
+        "👍": "https://library.penguinmod.com/files/emojis/thumbsup.png",
+        "👎": "https://library.penguinmod.com/files/emojis/thumbsdown.png",
+        "😱": "https://library.penguinmod.com/files/emojis/gladlyshocked.png",
+        "🫥": "https://library.penguinmod.com/files/emojis/dotted.png",
+        "😕": "https://library.penguinmod.com/files/emojis/confused.png",
+        "🥹": "https://library.penguinmod.com/files/emojis/holdingbacktears.png",
+        "🤝": "https://library.penguinmod.com/files/emojis/handshake.png",
+        "💥": "https://library.penguinmod.com/files/emojis/explode.png",
+        "😮": "https://library.penguinmod.com/files/emojis/surprised.png",
+        "🔓": "https://library.penguinmod.com/files/emojis/unlocked.png",
+        "🔒": "https://library.penguinmod.com/files/emojis/lock.png",
+    };
+
+    function walk(node) {
+        let child, next;
+        switch (node.nodeType) {
+            case 1:
+            case 9:
+            case 11:
+                child = node.firstChild;
+                while (child) {
+                    next = child.nextSibling;
+                    walk(child);
+                    child = next;
+                }
+                break;
+            case 3:
+                replaceInTextNode(node);
+                break;
+        }
+    }
+
+    function replaceInTextNode(textNode) {
+        const parent = textNode.parentNode;
+        let text = textNode.nodeValue;
+        const pattern = new RegExp(Object.keys(emojiMap).join('|'), 'g');
+        if (pattern.test(text)) {
+            const newHTML = text.replace(pattern, (match) =>
+                `<img src="${emojiMap[match]}" style="width:1em; height:1em; vertical-align:middle;">`);
+            const span = document.createElement('span');
+            span.innerHTML = newHTML;
+            while (span.firstChild) {
+                parent.insertBefore(span.firstChild, textNode);
+            }
+            parent.removeChild(textNode);
+        }
+    }
+
+    // Run emoji replacer once on page load
+    walk(document.body);
+
+    // Re-run when DOM changes
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            for (const node of mutation.addedNodes) {
+                walk(node);
+            }
+        });
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+})();
